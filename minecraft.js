000ea07e78860ac36be191258899460a89fce0e3
listgaus <listgaus@gmail.com>
2017-01-04 16:44:16 +0200

    Update minecraft.js
    added my section of tool box

--- minecraft.js
+++ minecraft.js
@@ -1,5 +1,18 @@
 var main = $(".container");
+var tool = $(".toolBox");
+
+//creating tool box divs
+var axe = $("<div id='axe' class='tool'></div>");
+var shovel = $("<div id='shovel' class='tool'></div>");
+var pickaxe =  $("<div id='pickaxe' class='tool'></div>");
+var selectedBrick = $("<div id='selectedBrick'></div>");
+
+tool.append(pickaxe);
+tool.append(shovel);
+tool.append(axe);
+tool.append(selectedBrick);
 var rowArray = [];
+
 
 //creating the grid
 for (var i = 0; i < 20; i++) {
@@ -21,69 +34,70 @@ console.log(rowArray);
 for (var i = 0; i <20; i++){
     for (var j = 0; j< 20; j++) {
         $("#"+i+"-"+j).css("background-color", "skyblue");
-        }
+    }
 }
 
-//setting that dirt 
+//setting that dirt
 for (var i = 14; i<20; i++){
     for (var j = 0; j < 20; j++){
-         $("#"+i+"-"+j).css("background-image", "url('./images/dirt.png')").css("background-size","cover");
+        $("#"+i+"-"+j).css("background-image", "url('./images/dirt.png')").css("background-size","cover");
     }
 }
 
-//time for some grass 
+//time for some grass
 for (var i = 13; i<14; i++){ //just want one row
     for (var j = 0; j < 20; j++){
-         $("#"+i+"-"+j).css("background-image", "url('./images/grass.png')").css("background-size","cover");
+        $("#"+i+"-"+j).css("background-image", "url('./images/grass.png')").css("background-size","cover");
     }
 }
 
-//adding left bush 
+//adding left bush
 for (var i = 12; i <13; i++){
     for (var j = 3; j<6; j++){
-         $("#"+i+"-"+j).css("background-image", "url('./images/leaf.jpg')").css("background-size","cover");
+        $("#"+i+"-"+j).css("background-image", "url('./images/leaf.jpg')").css("background-size","cover");
     }
 }
 
-//some more bush 
-         $("#"+11+"-"+4).css("background-image", "url('./images/leaf.jpg')").css("background-size","cover");
+//some more bush
+$("#"+11+"-"+4).css("background-image", "url('./images/leaf.jpg')").css("background-size","cover");
 
-//rocks 
-         $("#"+12+"-"+14).css("background-image", "url('./images/rock.png')").css("background-size","cover");
-         $("#"+12+"-"+13).css("background-image", "url('./images/rock.png')").css("background-size","cover");
-         $("#"+12+"-"+19).css("background-image", "url('./images/rock.png')").css("background-size","cover");
+//rocks
+$("#"+12+"-"+14).css("background-image", "url('./images/rock.png')").css("background-size","cover");
+$("#"+12+"-"+13).css("background-image", "url('./images/rock.png')").css("background-size","cover");
+$("#"+12+"-"+19).css("background-image", "url('./images/rock.png')").css("background-size","cover");
 
 //tree bark
-         $("#"+12+"-"+16).css("background-image", "url('./images/tree.png')").css("background-size","cover");
-         $("#"+11+"-"+16).css("background-image", "url('./images/tree.png')").css("background-size","cover");
-         $("#"+10+"-"+16).css("background-image", "url('./images/tree.png')").css("background-size","cover");
+$("#"+12+"-"+16).css("background-image", "url('./images/tree.png')").css("background-size","cover");
+$("#"+11+"-"+16).css("background-image", "url('./images/tree.png')").css("background-size","cover");
+$("#"+10+"-"+16).css("background-image", "url('./images/tree.png')").css("background-size","cover");
 
 //leaf for tree
-        for(var i = 7; i <10; i++){
-            for(var j = 15; j< 18; j++){
-         $("#"+i+"-"+j).css("background-image", "url('./images/leaf.jpg')").css("background-size","cover");
-            }
-        }
-
-//creating the cloud 
-    for (var i = 6; i <7; i++){
-        for ( var j = 3; j < 11; j++){
-         $("#"+i+"-"+j).css("background-color", "white");
-
-        }
+for(var i = 7; i <10; i++){
+    for(var j = 15; j< 18; j++){
+        $("#"+i+"-"+j).css("background-image", "url('./images/leaf.jpg')").css("background-size","cover");
     }
+}
+
+//creating the cloud
+for (var i = 6; i <7; i++){
+    for ( var j = 3; j < 11; j++){
+        $("#"+i+"-"+j).css("background-color", "white");
+
+    }
+}
 //some more of the cloud...
-         $("#"+7+"-"+8).css("background-color", "white");
-         $("#"+7+"-"+7).css("background-color", "white");
+$("#"+7+"-"+8).css("background-color", "white");
+$("#"+7+"-"+7).css("background-color", "white");
 
-         $("#"+5+"-"+10).css("background-color", "white");
-         $("#"+5+"-"+9).css("background-color", "white");
+$("#"+5+"-"+10).css("background-color", "white");
+$("#"+5+"-"+9).css("background-color", "white");
 
-         $("#"+5+"-"+4).css("background-color", "white");
-         $("#"+5+"-"+5).css("background-color", "white");
-         $("#"+5+"-"+6).css("background-color", "white");
-         $("#"+5+"-"+7).css("background-color", "white");
-         $("#"+4+"-"+6).css("background-color", "white");
+$("#"+5+"-"+4).css("background-color", "white");
+$("#"+5+"-"+5).css("background-color", "white");
+$("#"+5+"-"+6).css("background-color", "white");
+$("#"+5+"-"+7).css("background-color", "white");
+$("#"+4+"-"+6).css("background-color", "white");
+
 
 
 
