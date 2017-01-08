677998b624eb460553eb5e788cfe778a3b0dc086
listgaus <listgaus@gmail.com>
2017-01-08 18:36:28 +0200

    Add files via upload

--- minecraft.js
+++ minecraft.js
@@ -1,34 +1,34 @@
 var Minecraft = {};
-Minecraft.selectedTool = "";
+
+Minecraft.selectedTool = "";  //tool that has been selected
 Minecraft.selectedDiv = "";
-Minecraft.init = function(){
-  $(document).ready(function(){
+Minecraft.divPaste = ""; //this will be used as a memory slot for the copy/pasting of a class/brick
+
+Minecraft.init = function () {
+    $(document).ready(function () {
         Minecraft.start();
-  });
+    });
 };
 
 Minecraft.init();
 
-Minecraft.start = function(){
+Minecraft.start = function () {
     var main = $(".container");
     var tool = $(".toolBox");
 
 
 //creating tool box divs
-    var axe = $("<div id='axe' class='tool tree leaf'></div>");
+    var axe = $("<div id='axe' class='tool'></div>");
     axe.click(Minecraft.clickedTool);
 
-    var shovel = $("<div id='shovel' class='tool dirt grass'></div>");
+    var shovel = $("<div id='shovel' class='tool'></div>");
     shovel.click(Minecraft.clickedTool);
 
-    var pickaxe = $("<div id='pickaxe' class='tool brick'></div>");
+    var pickaxe = $("<div id='pickaxe' class='tool'></div>");
     pickaxe.click(Minecraft.clickedTool);
 
     var selectedBrick = $("<div id='selectedBrick'></div>");
     selectedBrick.click(Minecraft.copyPaste);
-
-
-
 
     tool.append(pickaxe);
     tool.append(shovel);
@@ -39,36 +39,35 @@ Minecraft.start = function(){
     //creating the grid
 
 
-
-    console.log(rowArray);
+    // console.log(rowArray);
 
 // creating the world in matrix gilads example
-var world = [   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                [0,0,0,0,1,1,1,1,0,1,1,0,0,0,0,0,0,0,0,0],
+    var world = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 
-                [0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,2,2,2,0,0],
-                [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,0,0],
-                [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,0,0],
-                [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0],
+        [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0],
 
-                [0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0],
-                [0,0,0,2,2,2,0,0,0,0,0,0,0,4,4,0,3,0,0,4],
-                [5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5],
-                [6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
-                [6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
-                
-                [6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
-                [6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
-                [6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
-                [6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
-                [6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
-]
+        [0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0],
+        [0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 4, 4, 0, 3, 0, 0, 4],
+        [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+        [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+        [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
 
-var blocks = ["sky","cloud","leaf","tree","rock","grass","dirt"];
+        [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+        [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+        [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+        [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+        [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+    ]
+
+    var blocks = ["sky", "cloud", "leaf", "tree", "rock", "grass", "dirt"];
 
     for (var i = 0; i < 20; i++) {
         var row = $("<div class='row'></div>");
@@ -86,77 +85,6 @@ var blocks = ["sky","cloud","leaf","tree","rock","grass","dirt"];
     }
 
 
-
-// // this function is the tool selector and updator
-Minecraft.clickedTool = function () {
-    if (Minecraft.selectedTool === "") {
-        Minecraft.selectedTool = $(this);
-        console.log(Minecraft.selectedTool);
-        $(this).addClass("clicked");
-    }
-   else if (Minecraft.selectedTool !== "") {
-       Minecraft.selectedTool.removeClass("clicked");
-        Minecraft.selectedTool = "";
-        Minecraft.selectedTool = $(this);
-        console.log(Minecraft.selectedTool);
-        $(this).css("background-color", "blue");
-    }
-}
-
-
-Minecraft.pickedDiv =function () {
-    if (Minecraft.selectedTool === "") {
-        return;
-    }
-    else if (Minecraft.selectedTool !== "") {
-        Minecraft.selectedDiv = $(this);
-        //alert(Minecraft.selectedDiv.classList);
-        //alert(Minecraft.selectedTool.classList);
-        console.log(Minecraft.selectedDiv.attr("class"));
-        console.log(Minecraft.selectedTool.attr("id"));
-        //tree and leaf for axe
-         //is means everything you ask, you dont need to say class or id
-        if (Minecraft.selectedDiv.is(".tree") && Minecraft.selectedTool.is("#axe")){
-            Minecraft.selectedDiv.removeClass("tree");
-            $("#selectedBrick").removeClass();
-            $("#selectedBrick").addClass("tree");
-
-        } 
-
-        if (Minecraft.selectedDiv.is(".leaf") && Minecraft.selectedTool.is("#axe")){
-            Minecraft.selectedDiv.removeClass("leaf");
-            $("#selectedBrick").removeClass();
-            $("#selectedBrick").addClass("leaf");
-            //not sure but think that its not showing in the selected brick div because its a gif
-        }
-
-        if (Minecraft.selectedDiv.is(".rock") && Minecraft.selectedTool.is("#pickaxe")){
-            Minecraft.selectedDiv.removeClass("rock");
-            $("#selectedBrick").removeClass();
-            $("#selectedBrick").addClass("rock");
-        }
-
-        if (Minecraft.selectedDiv.is(".dirt") && Minecraft.selectedTool.is("#shovel")){
-            Minecraft.selectedDiv.removeClass("dirt");
-            $("#selectedBrick").removeClass();
-            $("#selectedBrick").addClass("dirt");
-
-        } 
-
-        if (Minecraft.selectedDiv.is(".grass") && Minecraft.selectedTool.is("#shovel"))  {
-            Minecraft.selectedDiv.removeClass("grass");
-            $("#selectedBrick").removeClass();
-            $("#selectedBrick").addClass("grass");
-
-        }
-
-        if(Minecraft.selectedDiv.classList == Minecraft.selectedTool.classList){
-            $("#selectedBrick").css("background-color", "red");
-            return;
-        }
-        }
-}
-
 //now wana use the selected brick, make selected brick a namespace?
 
 // Minecraft.copyPaste = function(e){
@@ -173,6 +101,120 @@ Minecraft.pickedDiv =function () {
 
 }
 
+
+// this function is the tool selector and updator
+Minecraft.clickedTool = function () {
+    if (Minecraft.selectedTool === "") {
+        Minecraft.selectedTool = $(this);
+        // console.log(Minecraft.selectedTool);
+        $(this).addClass("clicked");
+        console.log(this);
+    }
+    else if (Minecraft.selectedTool !== "") {
+        Minecraft.selectedTool.removeClass("clicked");
+        Minecraft.selectedTool = $(this);
+        // console.log(Minecraft.selectedTool);
+        $(this).addClass("clicked");
+    }
+}
+
+
+Minecraft.pickedDiv = function () {
+
+    if ((Minecraft.selectedTool === "") && (Minecraft.divPaste !== "") && $(this).has("background-color", "skyblue")) {
+        Minecraft.selectedDiv = $(this);
+//this if term is for the paste parts, checking that -i have a selected tool,the background image is empty and paste memory isnt.
+        if (Minecraft.selectedDiv.is(".cloud") || Minecraft.selectedDiv.is(".leaf") || Minecraft.selectedDiv.is(".rock") || Minecraft.selectedDiv.is(".tree") || Minecraft.selectedDiv.is(".dirt") || Minecraft.selectedDiv.is(".grass")) {
+            return;
+        }
+        else {
+            var paste = $("#selectedBrick").attr('class');
+            console.log(paste);
+            var hello = $(this);
+            console.log(hello);
+            $(this).addClass(paste);
+
+            $("#selectedBrick").removeClass(paste);
+            $("#selectedBrick").removeClass("clicked");
+            Minecraft.divPaste = "";
+
+        }
+    }
+    else if ((Minecraft.selectedTool !== "") && (Minecraft.divPaste == "")) {
+
+        Minecraft.selectedDiv = $(this);
+
+        if (Minecraft.selectedDiv.is(".tree") && Minecraft.selectedTool.is("#axe")) {
+            Minecraft.selectedDiv.removeClass("tree");
+            Minecraft.selectedDiv.addClass("sky");
+            $("#selectedBrick").removeClass();
+            $("#selectedBrick").addClass("tree");
+
+        }
+
+        if (Minecraft.selectedDiv.is(".leaf") && Minecraft.selectedTool.is("#axe")) {
+
+            Minecraft.selectedDiv.removeClass("leaf");
+            Minecraft.selectedDiv.addClass("sky");
+            $("#selectedBrick").removeClass();
+            $("#selectedBrick").addClass("leaf");
+        }
+
+        if (Minecraft.selectedDiv.is(".rock") && Minecraft.selectedTool.is("#pickaxe")) {
+
+            Minecraft.selectedDiv.removeClass("rock");
+            Minecraft.selectedDiv.addClass("sky");
+            $("#selectedBrick").removeClass();
+            $("#selectedBrick").addClass("rock");
+
+        }
+
+        if (Minecraft.selectedDiv.is(".dirt") && Minecraft.selectedTool.is("#shovel")) {
+
+            Minecraft.selectedDiv.removeClass("dirt");
+            Minecraft.selectedDiv.addClass("sky");
+            $("#selectedBrick").removeClass();
+            $("#selectedBrick").addClass("dirt");
+        }
+
+        if (Minecraft.selectedDiv.is(".grass") && Minecraft.selectedTool.is("#shovel")) {
+
+            Minecraft.selectedDiv.removeClass("grass");
+            Minecraft.selectedDiv.addClass("sky");
+            $("#selectedBrick").removeClass();
+            $("#selectedBrick").addClass("grass");
+        }
+        //
+        // else  {
+        //
+        //     Minecraft.selectedTool.addClass("wrong");
+        //     setTimeout(function () {
+        //         Minecraft.selectedTool.removeClass("wrong");
+        //         // Minecraft.selectedTool.addClass("clicked");
+        //     }, 200);
+        //
+        // }
+
+    }
+}
+
+
+Minecraft.copyPaste = function () {
+    if (Minecraft.divPaste === "") {
+        $(".clicked").removeClass("clicked");
+        Minecraft.selectedTool = "";
+        $("#selectedBrick").addClass("clicked");
+        console.log(Minecraft.selectedTool);
+        console.log(this);
+        Minecraft.divPaste = this;
+
+        // console.log(Minecraft.divPaste);
+    }
+    else {
+
+        return;
+    }
+}
 
 
 
